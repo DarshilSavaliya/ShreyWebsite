bbcaa59842aa2842281239ade6d3530ce87dc07a
DARSHIL SAVALIYA <65533528+DarshilSavaliya@users.noreply.github.com>
2025-04-28 02:45:53 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,7 @@
+// mobile menu toggle
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('nav');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('open');
+});
